67d856e6b6619187dacfa94aa19ec70d8efad5f6
zhanghang <zhanghang3388@gmail.com>
2025-03-09 09:42:01 +0800

    commit

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,3 @@
-export default function Customers(){
+export default function Page(){
     return <p>Customers Page</p>
 }
--- app/ui/fonts.ts
+++ app/ui/fonts.ts
@@ -1,4 +1,8 @@
 
-import { Inter,Lusitana } from 'next/font/google';
-export const  inter = Inter({subsets:['latin']})
-export const lusitana = Lusitana({weight:"700"})
+import { Inter, Lusitana } from 'next/font/google';
+export const inter = Inter({ subsets: ['latin'] });
+
+export const lusitana = Lusitana({
+    weight: ['400', '700'],
+    subsets: ['latin'],
+  });
