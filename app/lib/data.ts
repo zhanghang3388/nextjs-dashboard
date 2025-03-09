c08ce8111f83ede1d52ba52f75c1015d3247a244
zhanghang <zhanghang3388@gmail.com>
2025-03-09 14:41:14 +0800

    'commit'

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -18,9 +18,7 @@ export async function fetchRevenue() {
 
     // console.log('Fetching revenue data...');
     // await new Promise((resolve) => setTimeout(resolve, 3000));
-
     const data = await sql<Revenue[]>`SELECT * FROM revenue`;
-
     // console.log('Data fetch completed after 3 seconds.');
 
     return data;
